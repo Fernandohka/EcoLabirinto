574ed57ef2d9e96f64b24a75d3335c82cf2dfba7
Fernandohka <fernando2005andrade@gmail.com>
2026-09-14 21:29:07 -0300

    godot

---
+++ Player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("A", "D", "W", "S");
+	
+	if direction:
+		velocity = direction * SPEED
+		if(Input.is_action_pressed("Shift")):
+			velocity *= 3
+	else:
+		velocity = Vector2.ZERO;
+	
+	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var objeto_colidido = collision.get_collider()
+		
+		if (objeto_colidido.has_method("setFire") and objeto_colidido.isOnFire == true):
+			objeto_colidido.setFire(false)
---
+++ Walls/wall.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+var isOnFire: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if(randi() % 100 < 20):
+		setFire(true)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func setFire(onFire: bool) -> void:
+	if (onFire):
+		isOnFire = true
+		$Sprite2D.modulate = Color(1.0, 0.0, 0.0, 1.0)
+	else:
+		isOnFire = false
+		$Sprite2D.modulate = Color(0.435, 1.0, 0.239)
